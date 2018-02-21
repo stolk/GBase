feed728b8c3743407ebbf99037e89f453d2fdfba
Bram Stolk <b.stolk@gmail.com>
2018-02-21 11:28:17 -0800

    Use OES variants for iOS build.

--- src/checkogl.h
+++ src/checkogl.h
@@ -15,6 +15,9 @@
 #		include <TargetConditionals.h>
 #		include <OpenGLES/ES2/gl.h>
 #		include <OpenGLES/ES2/glext.h>
+#		define glGenVertexArrays glGenVertexArraysOES
+#		define glDeleteVertexArrays glDeleteVertexArraysOES
+#		define glBindVertexArray glBindVertexArrayOES
 #	else
 #		include <GLES2/gl2.h>
 #		include <GLES2/gl2ext.h>
