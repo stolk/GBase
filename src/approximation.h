87612af854a92a7f1c1228a5d7b558952a66e9da
stolk <b.stolk@gmail.com>
2019-01-28 10:40:24 -0800

    Add logf approximation.

--- src/approximation.h
+++ src/approximation.h
@@ -54,6 +54,20 @@ static inline void sincos_approximation( float x, float& sine, float& cosine )
 }
 #endif
 
+static inline float sin_approximation(float x)
+{
+	const float fouroverpi = 4.0f / M_PI;
+	const float fouroverpisquare = 4.0f / (M_PI * M_PI);
+
+	x = fmodf(x, M_PI * 2.0f);
+	x = x > M_PI ? x - 2.0f * M_PI : x; // map to -pi .. pi
+
+	if (x < 0.0f)
+		return fouroverpi * x + fouroverpisquare * x * x;
+	else
+		return fouroverpi * x - fouroverpisquare * x * x;
+}
+
 // natural log on [0x1.f7a5ecp-127, 0x1.fffffep127]. Maximum relative error 9.4529e-5
 // From: https://stackoverflow.com/a/39822314/301166
 static inline int __float_as_int( float in )
@@ -70,20 +84,21 @@ static inline float __int_as_float( int in )
 }
 float logf_approximation( float a )
 {
-    float m, r, s, t, i, f;
-    int32_t e;
+	float m, r, s, t, i, f;
+	int32_t e;
 
-    e = (__float_as_int (a) - 0x3f2aaaab) & 0xff800000;
-    m = __int_as_float (__float_as_int (a) - e);
-    i = (float)e * 1.19209290e-7f; // 0x1.0p-23
-    /* m in [2/3, 4/3] */
-    f = m - 1.0f;
-    s = f * f;
-    /* Compute log1p(f) for f in [-1/3, 1/3] */
-    r = fmaf (0.230836749f, f, -0.279208571f); // 0x1.d8c0f0p-3, -0x1.1de8dap-2
-    t = fmaf (0.331826031f, f, -0.498910338f); // 0x1.53ca34p-2, -0x1.fee25ap-2
-    r = fmaf (r, s, t);
-    r = fmaf (r, s, f);
-    r = fmaf (i, 0.693147182f, r); // 0x1.62e430p-1 // log(2)
-    return r;
+	e = (__float_as_int (a) - 0x3f2aaaab) & 0xff800000;
+	m = __int_as_float (__float_as_int (a) - e);
+	i = (float)e * 1.19209290e-7f; // 0x1.0p-23
+	/* m in [2/3, 4/3] */
+	f = m - 1.0f;
+	s = f * f;
+	/* Compute log1p(f) for f in [-1/3, 1/3] */
+	r = fmaf (0.230836749f, f, -0.279208571f); // 0x1.d8c0f0p-3, -0x1.1de8dap-2
+	t = fmaf (0.331826031f, f, -0.498910338f); // 0x1.53ca34p-2, -0x1.fee25ap-2
+	r = fmaf (r, s, t);
+	r = fmaf (r, s, f);
+	r = fmaf (i, 0.693147182f, r); // 0x1.62e430p-1 // log(2)
+	return r;
 }
+
