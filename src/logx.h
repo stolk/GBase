9b75540e46a71d6b42a947c994619e391b8fa1c9
stolk <b.stolk@gmail.com>
2019-09-16 12:23:50 -0700

    Add indentation.

--- src/logx.h
+++ src/logx.h
@@ -24,74 +24,84 @@ typedef void(*asserthook_t) (const char* condition, const char* file, int line);
 extern asserthook_t asserthook;
 extern FILE* logx_file;
 
+/*
+ * Define LOGI, LOGW, LOGE macros.
+ */
+
 #if defined(ANDROID)
 
-#include <android/log.h>
-#include <assert.h>
+#	include <android/log.h>
+#	include <assert.h>
 
-#if !defined(LOGTAG)
-#define LOGTAG base
-#endif
+#	if !defined(LOGTAG)
+#		define LOGTAG base
+#	endif
 
-#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO,  TOSTRING(LOGTAG), __VA_ARGS__))
-#define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN,  TOSTRING(LOGTAG), __VA_ARGS__))
-#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, TOSTRING(LOGTAG), __VA_ARGS__))
+#	define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO,  TOSTRING(LOGTAG), __VA_ARGS__))
+#	define LOGW(...) ((void)__android_log_print(ANDROID_LOG_WARN,  TOSTRING(LOGTAG), __VA_ARGS__))
+#	define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, TOSTRING(LOGTAG), __VA_ARGS__))
 
 #else
 
-#include <assert.h>
+#	include <assert.h>
 
-#define LOGI(...) \
-{ \
-  printf( __VA_ARGS__ ); \
-  printf("\n"); \
-  fflush( stdout ); \
-  if ( logx_file ) { fprintf( logx_file, __VA_ARGS__ ); fprintf( logx_file, "\n" ); fflush(logx_file); } \
-}
+#	define LOGI(...) \
+	{ \
+		printf( __VA_ARGS__ ); \
+		printf("\n"); \
+		fflush( stdout ); \
+		if ( logx_file ) { fprintf( logx_file, __VA_ARGS__ ); fprintf( logx_file, "\n" ); fflush(logx_file); } \
+	}
 
-#define LOGW(...) \
-{ \
-  printf( WRNFMT __VA_ARGS__ ); \
-  printf("\n"); \
-  fflush( stdout ); \
-  if ( logx_file ) { fprintf( logx_file, "WRN " __VA_ARGS__ ); fprintf( logx_file, "\n" ); fflush(logx_file); } \
-}
+#	define LOGW(...) \
+	{ \
+		printf( WRNFMT __VA_ARGS__ ); \
+		printf("\n"); \
+		fflush( stdout ); \
+		if ( logx_file ) { fprintf( logx_file, "WRN " __VA_ARGS__ ); fprintf( logx_file, "\n" ); fflush(logx_file); } \
+	}
 
-#define LOGE(...) \
-{ \
-  printf( ERRFMT __VA_ARGS__ ); \
-  printf("\n"); \
-  fflush( stdout ); \
-  if ( logx_file ) { fprintf( logx_file, "ERR " __VA_ARGS__ ); fprintf( logx_file, "\n" ); fflush(logx_file); } \
-}
-
-
+#	define LOGE(...) \
+	{ \
+		printf( ERRFMT __VA_ARGS__ ); \
+		printf("\n"); \
+		fflush( stdout ); \
+		if ( logx_file ) { fprintf( logx_file, "ERR " __VA_ARGS__ ); fprintf( logx_file, "\n" ); fflush(logx_file); } \
+	}
 #endif
+
+
+/*
+ * Define ASSERT macros.
+ */
+
 
 #define ASSERTM(C, FMT, ...) \
 { \
-if ( !(C) ) \
-{\
-char assertmessage[1024]; \
-snprintf( assertmessage, sizeof(assertmessage), "%s / " FMT, #C, __VA_ARGS__ ); \
-LOGE("ASSERT FAILED at %s(%d): %s", __BASE_FILE__, __LINE__, assertmessage); \
-if ( asserthook ) asserthook( assertmessage, __BASE_FILE__, __LINE__ ); \
-}\
-assert( C ); \
+	if ( !(C) ) \
+	{\
+		char assertmessage[1024]; \
+		snprintf( assertmessage, sizeof(assertmessage), "%s / " FMT, #C, __VA_ARGS__ ); \
+		LOGE("ASSERT FAILED at %s(%d): %s", __BASE_FILE__, __LINE__, assertmessage); \
+		if ( asserthook ) asserthook( assertmessage, __BASE_FILE__, __LINE__ ); \
+	}\
+	assert( C ); \
 }
+
 
 #define ASSERT(C) \
 { \
-if ( !(C) ) \
-{ \
-LOGE( "ASSERT FAILED at %s(%d):  %s", __BASE_FILE__, __LINE__, #C ); \
-if ( asserthook ) asserthook( #C, __BASE_FILE__, __LINE__ ); \
-} \
-assert( C ); \
+	if ( !(C) ) \
+	{ \
+		LOGE( "ASSERT FAILED at %s(%d):  %s", __BASE_FILE__, __LINE__, #C ); \
+		if ( asserthook ) asserthook( #C, __BASE_FILE__, __LINE__ ); \
+	} \
+	assert( C ); \
 }
 
+
 #ifdef __cplusplus
-};
+}
 #endif
 
 
