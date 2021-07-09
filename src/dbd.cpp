466cf17817e559a858b7c71c06b5cb3e1d2ada76
Bram Stolk <b.stolk@gmail.com>
2021-07-08 18:39:30 -0700

    Add circle drawing in y plane.

--- src/dbd.cpp
+++ src/dbd.cpp
@@ -125,6 +125,24 @@ void dbd_circlex( const mat44_t& m )
 }
 
 
+void dbd_circley( const mat44_t& m )
+{
+	const int nump = 12;
+	vec4_t pts[ nump ];
+	for ( int i=0; i<nump; ++i )
+	{
+		const float a = M_PI * 2.0f * i / (nump-1.0f);
+		pts[ i ] = vec4_t( cosf(a), 0.0f, sinf(a), 1.0f );
+	}
+	for ( int i=0; i<nump-1; ++i )
+	{
+		const vec4_t xp0 = m * pts[ i+0 ];
+		const vec4_t xp1 = m * pts[ i+1 ];
+		dbd_line( vec3_t(xp0[0],xp0[1],xp0[2]), vec3_t(xp1[0],xp1[1],xp1[2]) );
+	}
+}
+
+
 void dbd_circlez( const mat44_t& m )
 {
 	const int nump = 12;
--- src/dbd.h
+++ src/dbd.h
@@ -22,6 +22,8 @@ extern void dbd_corners( const vec3_t& lo, const vec3_t& hi );
 
 extern void dbd_circlex( const mat44_t& m );
 
+extern void dbd_circley( const mat44_t& m );
+
 extern void dbd_circlez( const mat44_t& m );
 
 extern void dbd_sphere( const vec3_t pos, float r );
